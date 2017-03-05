3ef01726a6abe5f618612c0b868396b06290ce57
Charles Dunn <dunnc1551@gmail.com>
2017-03-04 19:51:28 -0700

    Add things to do banner with images

---
+++ script.js
@@ -0,0 +1,9 @@
+
+$('.menu-btn, .close-menu-btn').on('click', function() {
+  $('.main-nav').slideToggle();
+});
+
+// $('.wrapper').on('scroll', function(e) {
+//   $('.main-nav').css({"background-color": "#7a1c1f"});
+//   $('.menu a').css({"color": "#fff"});
+// });
