25e58d6069f85493b6a6a86797e2f8810952a529
Charles Dunn <dunnc1551@gmail.com>
2017-03-06 13:42:31 -0700

    Change nav bar display on desktop to hidden on load

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ $('.menu-btn, .close-menu-btn').on('click', function() {
 });
 
 $( window ).on('scroll', function(e) {
-  if ($(this).width() > 750 && $(this).scrollTop() > 400) {
+  if ($(this).width() > 821 && $(this).scrollTop() > 300) {
+    $('.main-nav').css("display", "block");
     $('.main-nav').addClass('fixed');
     $('.menu a').css("color", "#e5dada");
+    $('.home-icon').css("display", "flex");
   }
 
-  if ($(this).scrollTop() < 200) {
-    $('.main-nav').removeClass('fixed');
+  if ($(this).width() > 821 && $(this).scrollTop() < 200) {
+    $('.main-nav').css("display", "none");
     $('.menu a').css("color", "#838383");
+    $('.home-icon').css("display", "flex");
   }
-
 });
